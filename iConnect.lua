4e6465151b66fa8956395f9e5dde16f8ae46b445
Yannick <YannickZiermann@gmx.de>
2014-11-24 08:32:00 +0100

    Added MySQL class

--- iConnect.lua
+++ iConnect.lua
@@ -10,11 +10,34 @@ wcf.__index = wcf
 
 function wcf:connect(host, username, password, database)
 	local c = {}
-	c.hCon = dbConnect("mysql", ("dbname=%s;host=%s"):format(database, host), username, password, "autoreconnect=1")
+	c.hCon = mysql:connect("mysql", ("dbname=%s;host=%s"):format(database, host), username, password, "autoreconnect=1")
     setmetatable(c, wcf)
     return c
 end
 
 function wcf:disconnect()
 	destroyElement(self.hCon)
+end
+
+---
+--MySQL
+---
+mysql = {}
+mysql.__index = mysql
+
+function mysql:connect(host, username, password, database)
+    local c = {}
+    c.hCon = dbConnect(host, username, password, database)
+    setmetatable(c, mysql)
+    return c
+end
+
+function mysql:query(q, ...)
+    local query = dbQuery(self.hCon, q, ...)
+    local result, qRows, qliID = dbPoll(query, 100)
+    if result == false then
+       return false
+    elseif result then
+        return result, qRows, qliID
+    else dbFree(query) end
 end
