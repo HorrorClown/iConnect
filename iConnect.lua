fd64bcf6a747685d85d6e6f3a0fb19d6a6f4cf17
Yannick <YannickZiermann@gmx.de>
2014-11-27 00:08:56 +0100

    Added classlib
    Classlib by sbx320

--- iConnect.lua
+++ iConnect.lua
@@ -5,39 +5,54 @@
 -- License: MIT/X11
 -- pewx.de // iGaming-mta.de // iRace-mta.de // iSurvival.de // mtasa.de
 --
-wcf = {}
-wcf.__index = wcf
+Cwbbc = {}
 
-function wcf:connect(host, username, password, database)
-	local c = {}
-	c.hCon = mysql:connect("mysql", ("dbname=%s;host=%s"):format(database, host), username, password, "autoreconnect=1")
-    setmetatable(c, wcf)
-    return c
+function Cwbbc:constructor(sHost, sUser, sPass, sDBName, sPort)
+    self.hCon = dbConnect("mysql", ("dbname=%s;host=%s;port=%s"):format(sDBName, sHost, sPort), sUser, sPass, "autoreconnect=1")
+    if self.hCon then
+        outputDebugString("|wbbc| Connected to mysql server!")
+    else
+        outputDebugString("|wbbc| Can't connect to  mysql server!")
+        stopResource(getThisResource())
+    end
 end
 
-function wcf:disconnect()
-	destroyElement(self.hCon)
+function Cwbbc:destructor()
+    destroyElement(self.hCon)
+    self.hCon = ni
 end
 
----
---MySQL
----
-mysql = {}
-mysql.__index = mysql
-
-function mysql:connect(host, username, password, database)
-    local c = {}
-    c.hCon = dbConnect(host, username, password, database)
-    setmetatable(c, mysql)
-    return c
+function Cwbbc:getUserID(sUsername)
+    if not self.hCon then self:message("Not connected to mysql server!") return false end
+    assert(type(sUsername) == "string", "Invalid string @ argument 1")
+    return self:get("wcf1_user", "userID", "username", sUsername)
 end
 
-function mysql:query(q, ...)
+function Cwbbc:query(q, ...)
     local query = dbQuery(self.hCon, q, ...)
     local result, qRows, qliID = dbPoll(query, 100)
     if result == false then
-       return false
+        return false
     elseif result then
         return result, qRows, qliID
     else dbFree(query) end
+end
+
+function Cwbbc:insert(t, c, v, ...)         --t = table | c = columns | v = values
+    return dbExec(self.hCon, ("INSERT INTO %s (%s) VALUES (%s)"):format(t, c, v), ...)
+end
+
+function Cwbbc:set(t, c, cV, w, wV)   		--t = table | c = column | cV = columnValue | w = where | wV = whereValue
+    return dbExec(self.hCon, "UPDATE ?? SET ??=? WHERE ??=?", t, c, cV, w, wV)
+end
+
+function Cwbbc:get(t, c, w, wV, wO, wVO)    --t = table | c = column | w = where | wV = whereValue | wO = whereOpational | wVO = whereValueOptional
+    local q, rs
+    if wO and wVO then q, rs = self:query(("SELECT %s FROM %s WHERE %s = '%s' AND %s = '%s'"):format(c, t, w, wV, wO, wVO)) else q, rs = self:query(("SELECT %s FROM %s WHERE %s = '%s'"):format(c, t, w, wV)) end
+    if not q then return false end
+    if rs > 1 then return q end
+
+    for _, row in ipairs(q) do
+        return row[c]
+    end
 end
