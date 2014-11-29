e88a3aab48b21b99469eeaf0456bff193161d356
Yannick Ziermann <yannickziermann@gmx.de>
2014-11-29 03:52:35 +0100

    Fixes

--- iConnect.lua
+++ iConnect.lua
@@ -10,7 +10,7 @@ Cwbbc = {}
 function Cwbbc:constructor(sHost, sUser, sPass, sDBName, sPort)
     self.sHost = sHost
     self.sUser = sUser
-    self.sDBname = sDBName
+    self.sDBName = sDBName
     self.hCon = dbConnect("mysql", ("dbname=%s;host=%s;port=%s"):format(sDBName, sHost, sPort), sUser, sPass, "autoreconnect=1")
     if self.hCon then
         self:message("Successfully connected!")
@@ -30,10 +30,12 @@ end
 function Cwbbc:comparePassword(sUsername, sPW)
 	if not self.hCon then self:message("Not connected to mysql server") return false end
 	assert(type(sUsername) == "string", "Invalid string @ argument 1")
-	assert(type(sUsername, sPW) == "string", "Invalid string @ argument 2")
+	assert(type(sPW) == "string", "Invalid string @ argument 2")
 	if self:get("wcf1_user", "username", "username", sUsername) then
 		local dbHash = self:get("wcf1_user", "password", "username", sUsername)
-		local pwHash = getDoubleSaltedHash(dbHash, sUsername, sPW)
+		local pwHash = getDoubleSaltedHash(dbHash, sPW)
+        outputChatBox("dbHash:" .. tostring(dbHash))
+        outputChatBox("pwHash:" .. tostring(pwHash))
 		return (dbHash == pwHash)
 	end
 	return false
@@ -42,13 +44,14 @@ end
 function Cwbbc:getUserID(sUsername)
     if not self.hCon then self:message("Not connected to mysql server!") return false end
     assert(type(sUsername) == "string", "Invalid string @ argument 1")
-    return tonumber(self:get("wcf1_user", "userID", "username", sUsername))
+    local qResult = self:get("wcf1_user", "userID", "username", sUsername)
+    if qResult ~= nil then return tonumber(qResult) else return false end
 end
 
 function Cwbbc:getUserName(nUID)
     if not self.hCon then self:message("Not connected to mysql server!") return false end
     assert((type(nUID) == "number"), "Invalid number @ argument 1")
-    return self:get("wcf1_user", "username", "userID", nUID)
+    return self:get("wcf1_user", "username", "userID", nUID) or false
 end
 
 function Cwbbc:getUserTitle(nUID)
@@ -195,7 +198,7 @@ end
 --//Useful
 
 function Cwbbc:message(sMessage)
-	outputDebugString(("[%s:%s@%s]: %s"):format(self.sUser, self.sHost, self.sPort, self.sDBName, sMessage), 0, 255, 0, 255)
+	outputDebugString(("[%s:%s@%s]: %s"):format(self.sUser, self.sHost, self.sDBName, sMessage), 0, 255, 0, 255)
 end
 
 function getDoubleSaltedHash(sDBHash, sPW)
